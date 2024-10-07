d2dbe809984bc1622b41042fb2290139739cde5b
arasan01 <developer@arasan01.com>
2024-10-08 05:35:05 +0900

    add dependencies

--- Package.swift
+++ Package.swift
@@ -11,11 +11,39 @@ let package = Package(
       name: "DNS",
       targets: ["DNS"])
   ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-nio.git", from: "2.74.0"),
+    .package(url: "https://github.com/swift-server/swift-service-lifecycle.git", from: "2.0.0"),
+    .package(url: "https://github.com/apple/swift-atomics.git", from: "1.0.0"),
+    .package(url: "https://github.com/apple/swift-async-algorithms.git", from: "1.0.0"),
+    .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0"),
+    .package(url: "https://github.com/apple/swift-metrics.git", from: "2.5.0"),
+    .package(url: "https://github.com/apple/swift-distributed-tracing.git", from: "1.0.1"),
+    .package(url: "https://github.com/apple/swift-collections.git", from: "1.0.0"),
+    .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.19.0"),
+  ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
     // Targets can depend on other targets in this package and products from dependencies.
     .target(
-      name: "DNS"),
+      name: "DNS",
+      dependencies: [
+        .product(name: "ServiceLifecycle", package: "swift-service-lifecycle"),
+        .product(name: "Atomics", package: "swift-atomics"),
+        .product(name: "Logging", package: "swift-log"),
+        .product(name: "Metrics", package: "swift-metrics"),
+        .product(name: "Tracing", package: "swift-distributed-tracing"),
+        .product(name: "AsyncAlgorithms", package: "swift-async-algorithms"),
+        .product(name: "Collections", package: "swift-collections"),
+        .product(name: "Logging", package: "swift-log"),
+        .product(name: "NIOCore", package: "swift-nio"),
+        .product(name: "NIOConcurrencyHelpers", package: "swift-nio"),
+        .product(name: "NIOExtras", package: "swift-nio-extras"),
+        .product(name: "NIOPosix", package: "swift-nio"),
+        .product(name: "NIOTransportServices", package: "swift-nio-transport-services"),
+        .product(name: "AsyncHTTPClient", package: "async-http-client"),
+      ]
+    ),
     .testTarget(
       name: "DNSTests",
       dependencies: ["DNS"]
